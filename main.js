b2d00fbb9d1dbde3451b3c2bfdbcaded8e6332e3
vitumeireles <87585413+vitumeireles@users.noreply.github.com>
2024-03-13 00:18:32 -0300

    new file:   .DS_Store 	new file:   .vscode/settings.json 	new file:   images/receiver.png 	new file:   index.html 	new file:   main.css 	new file:   main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form-contato');
+const contatos = [];
+const numeros = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
+
+    if(contatos.includes(inputNomeContato.value)){
+        alert (`O Contato ${inputNomeContato.value} já foi inserido`)
+    } else {
+
+    contatos.push(inputNomeContato.value);
+    numeros.push(inputNumeroContato.value);
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumeroContato.value}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+    }
+
+    inputNomeContato.value = '';
+    inputNumeroContato.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
